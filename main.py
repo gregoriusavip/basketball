81018c623f78dca37f336114ee89c5c69149314b
Gregorius Avip <71865513+gregoriusavip@users.noreply.github.com>
2026-05-28 10:41:10 -0700

    Add a separate delay between the timer and the basketball

--- main.py
+++ main.py
@@ -9,19 +9,26 @@ def countdown(seconds: number):
             display.show_number(0)
             break
         play_basketball()
+        basic.pause(20)
 
 def on_button_pressed_a():
     countdown(24)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def play_basketball():
-    global distance, total_score
+    global distance, total_score, next_ready
+    now = input.running_time()
+    if now < next_ready:
+        return
+    
     distance = PlanetX_Basic.ultrasound_sensor(PlanetX_Basic.DigitalRJPin.J2,
         PlanetX_Basic.Distance_Unit_List.DISTANCE_UNIT_CM)
     if distance > 0 and distance < THRESHOLD_CM:
         total_score += 2
         basic.show_number(total_score)
-        basic.pause(COOLDOWN_MS)
+        next_ready = now + COOLDOWN_MS
+
+next_ready = 0
 total_score = 0
 distance = 0
 start = 0
@@ -31,5 +38,5 @@ THRESHOLD_CM = 0
 # SETTINGS
 SHOT_CLOCK_SECONDS = 24
 THRESHOLD_CM = 8
-COOLDOWN_MS = 200
+COOLDOWN_MS = 800
 display = PlanetX_Display.tm1637_create(PlanetX_Display.DigitalRJPin.J1)
--- main.ts
+++ main.ts
@@ -11,21 +11,28 @@ start = input.runningTime()
             break;
         }
         play_basketball()
+        basic.pause(20)
     }
 }
 input.onButtonPressed(Button.A, function () {
     countdown(24)
 })
 function play_basketball () {
+    now = input.runningTime()
+    if (now < next_ready) {
+        return
+    }
     distance = PlanetX_Basic.ultrasoundSensor(PlanetX_Basic.DigitalRJPin.J2, PlanetX_Basic.Distance_Unit_List.Distance_Unit_cm)
     if (distance > 0 && distance < THRESHOLD_CM) {
         total_score += 2
         basic.showNumber(total_score)
-        basic.pause(COOLDOWN_MS)
+        next_ready = now + COOLDOWN_MS
     }
 }
 let total_score = 0
 let distance = 0
+let next_ready = 0
+let now = 0
 let start = 0
 let display: PlanetX_Display.TM1637LEDs = null
 let COOLDOWN_MS = 0
@@ -33,5 +40,5 @@ let THRESHOLD_CM = 0
 // SETTINGS
 let SHOT_CLOCK_SECONDS = 24
 THRESHOLD_CM = 8
-COOLDOWN_MS = 200
+COOLDOWN_MS = 800
 display = PlanetX_Display.tm1637Create(PlanetX_Display.DigitalRJPin.J1)
